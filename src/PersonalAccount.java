f10b7c926dffa86a9408178716511d3b722f0630
Aktilek <103207943+SelVord@users.noreply.github.com>
2023-09-23 19:23:10 +0600

    Add files via upload

---
+++ src/PersonalAccount.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+
+public class PersonalAccount {
+    private int accountNumber;
+    private String accountHolder;
+    private double balance;
+    private ArrayList<Amount> transactions;
+
+    // Constructor for PersonalAccount class
+
+    public PersonalAccount(int accountNumber, String accountHolder) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = 0.0;
+        this.transactions = new ArrayList<Amount>();
+    }
+
+    // Code to add deposit to account
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            transactions.add(new Amount(amount, "Deposit"));
+        }
+    }
+
+    // Code to withdraw money from account
+
+    public void withdraw(double amount) {
+        if (amount > 0 && balance >= amount) {
+            balance -= amount;
+            transactions.add(new Amount(amount, "Withdrawal"));
+        }
+    }
+
+    // Code shows all account history
+
+    public void printTransactionHistory() {
+        for (Amount transaction : transactions) {
+            System.out.println("Transaction Type: " + transaction.getTransactionType());
+            System.out.println("Amount: " + transaction.getAmount());
+            System.out.println();
+        }
+    }
+
+    // These codes allow to show proper data
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+}
